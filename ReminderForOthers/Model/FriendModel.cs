cea5018692aff83f876c108b8087bf7d50f2408f
Natraj Padwani <nextronickk@gmail.com>
2023-03-23 12:33:56 +0800

    Added Freinds Model and Made the Friends to Firebase realtime database like the SignUpModel.

---
+++ ReminderForOthers/Model/FriendModel.cs
@@ -0,0 +1,101 @@
+﻿using Firebase.Auth;
+using Firebase.Database;
+using Firebase.Database.Query;
+using FirebaseAdmin;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using ReminderForOthers.View;
+using static Android.Gms.Common.Apis.Api;
+
+namespace ReminderForOthers.Model
+{
+    public class FriendRequest
+    {
+        //user sending request
+        public string Username { get; set; }
+        //sent to user
+        public string FriendUsername { get; set; }
+        //accept [0: Neutral, 1: Accepted, -1: Declined]
+        public int Accept { get; set; }
+        //Request Creation Date
+        public DateTime RequestDate { get; set; }
+    }
+
+
+    internal class FriendModel
+    {
+        private const string Web_API_KEY = "AIzaSyDzaUbGPZSIoOknBEOLNOA5KbVGstQ1Sks";
+        private const string Database_URL = "https://reminderforothers-default-rtdb.asia-southeast1.firebasedatabase.app";
+        private FirebaseClient client = new FirebaseClient(Database_URL);
+
+        public async Task<bool> SentRequestAsync(FriendRequest friendRequest)
+        {
+            try
+            {
+               
+                //check if friend username exists
+                SignUpModel signUpModel = new SignUpModel();
+                bool exists = await signUpModel.DoesUserNameExits(friendRequest.FriendUsername);
+                if (!exists) { return false; }
+
+                //check if firends already
+                int isFriendsAlready = -2; //[0: current user sent request already, 1: already friends, -1: You already received request, -2: default ]
+
+                var friends = await client.Child(nameof(Friend)).OnceAsync<FriendRequest>();
+                foreach (var item in friends)
+                {
+                    Console.WriteLine("item key: "+item.Key);
+                    FriendRequest temp = item.Object;
+                    if (temp.Username == friendRequest.Username && temp.FriendUsername == friendRequest.FriendUsername)
+                    {
+                        if (temp.Accept == 1) { isFriendsAlready = 1; }
+                        else if (temp.Accept == 0) { isFriendsAlready = 0; }
+                    }
+                    else if (temp.Username == friendRequest.FriendUsername && temp.FriendUsername == friendRequest.Username)
+                    {
+                        if (temp.Accept == 1) { isFriendsAlready = 1; }
+                        else if (temp.Accept == 0) { isFriendsAlready = -1; } //other user already sent u request
+                    }
+                }
+
+                if (isFriendsAlready != -2) { return false; }
+
+                //not friends then add as friends
+                var response =  await client.Child(nameof(FriendRequest)).PostAsync(friendRequest);
+                Console.WriteLine("response Key: "+response.Key);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Error Occured", "Friend cannot be set due to: \n" + ex.Message, "Okay");
+            }
+            return false;
+        }
+
+        //private async Task<List<FriendRequest>> GetAllFriendRequestsAsync() {
+        //    var friends = await client.Child("Friends").OnceAsync<FriendRequest>();
+        //    return friends;
+        //}
+
+        public async Task<IDictionary<string,FriendRequest>> GetFriendRequestAsync(string username)
+        {
+            
+            var friends = await client.Child("Friends").OnceAsync<FriendRequest>();
+            IDictionary<string, FriendRequest> myFriends = new Dictionary<string,FriendRequest>();
+            foreach (var item in friends)
+            {
+               FriendRequest temp  =  item.Object;
+                if (temp.FriendUsername == username && temp.Accept == 0) {
+                    myFriends.Add(item.Key,temp);
+                } 
+            }
+            return myFriends;
+        }
+        //public async Task<bool> AcceptFriendRequestAsync(FriendRequest friendRequest) 
+        //{
+
+        //    FirebaseClient client = new FirebaseClient(Database_URL);
+
+        //}
+    }
+}
--- ReminderForOthers/ViewModel/FriendViewModel.cs
+++ ReminderForOthers/ViewModel/FriendViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using ReminderForOthers.Model;
 using ReminderForOthers.View;
 
 namespace ReminderForOthers.ViewModel
@@ -7,12 +8,78 @@ namespace ReminderForOthers.ViewModel
     public partial class FriendViewModel : ObservableObject
     {
         private string username;
+        private FriendModel friendModel;
+        private LoginModel loginModel;
+        public FriendViewModel() { 
+            friendModel = new FriendModel();
+            loginModel = new LoginModel();
+
+        }
 
         [RelayCommand]
         async void AddFriend()
         {
-            username = await Shell.Current.DisplayPromptAsync("Add Friend", "Write down the username of a friend.", keyboard: Keyboard.Text);
-            await Shell.Current.DisplayAlert("Friend Request Sent", "User '" + username + "' will have to accept the freind request in order to become your friend.", "Okay");
+            string placeHolder = "eg. John";
+            username = await Shell.Current.DisplayPromptAsync("Add Friend", "Write down the username of a friend.", keyboard: Keyboard.Text, placeholder: placeHolder);
+
+            //if they click cancel
+            if (username == null) { return; }
+
+            if (string.IsNullOrEmpty(username))
+            {
+                string[] btn = { "Yes", "No" };
+                bool val = await Shell.Current.DisplayAlert("Username Empty", "Username is empty, would you like to add again?", "Yes", "No");
+                Console.WriteLine("DisplayAlert: " + val);
+
+                if (!val) { return; }
+                AddFriend();
+                return; //to exit the code if it runs again.
+            }
+
+            SendFriendRequest(username);
+        }
+
+        //to add friends onto database for request
+        private async void SendFriendRequest(string username)
+        {
+            try
+            {
+                
+                FriendRequest friendRequest = new FriendRequest();
+                friendRequest.Username = await loginModel.GetLogInCacheAsync(); //gets logged in user info
+                friendRequest.FriendUsername = username;
+                friendRequest.Accept = 0;
+                friendRequest.RequestDate = DateTime.Now;
+
+                if (friendRequest.Username == username) {
+                    await Shell.Current.DisplayAlert("Friend Request Failed", "User \"" + username + "\" is your username.", "Okay");
+                    return;
+                }
+
+                bool response = await friendModel.SentRequestAsync(friendRequest);
+
+                if (!response)
+                {
+                    await Shell.Current.DisplayAlert("Friend Request Failed", "User \"" + username + "\" does not exists.", "Okay");
+                    return;
+                }
+
+                Console.WriteLine("Time RN: " + DateTime.Now);
+                Console.WriteLine("Response: " + response);
+
+                await Shell.Current.DisplayAlert("Friend Request Sent", "User \"" + username + "\" will have to accept the freind request in order to become your friend.", "Okay");
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlert("Friend Request Error", "User \"" + username + "\" friend request could not be sent.\n" + ex.Message, "Okay");
+
+            }
+        }
+
+        async Task<List<FriendRequest>> GetFriendRequestsAsync() 
+        {
+            return await friendModel.GetFriendRequestAsync(await loginModel.GetLogInCacheAsync());
+
         }
 
         [RelayCommand]
