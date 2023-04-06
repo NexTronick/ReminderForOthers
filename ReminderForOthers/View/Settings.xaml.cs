b0e2554f7b12abbde6804ac39c9a4acb459a0297
Natraj Padwani <nextronickk@gmail.com>
2023-04-07 01:37:03 +0800

    Added Some Settings logout funcitons.

--- ReminderForOthers/View/Settings.xaml.cs
+++ ReminderForOthers/View/Settings.xaml.cs
@@ -1,9 +1,12 @@
+using ReminderForOthers.ViewModel;
 namespace ReminderForOthers.View;
+
 
 public partial class Settings : ContentPage
 {
 	public Settings()
 	{
 		InitializeComponent();
-	}
+		BindingContext = new SettingsViewModel();
+    }
 }
---
+++ ReminderForOthers/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,32 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace ReminderForOthers.ViewModel
+{
+    public partial class SettingsViewModel : ObservableObject
+    {
+
+        [ObservableProperty]
+        string currentUser;
+
+        private MainViewModel mainViewModel;
+       
+        public SettingsViewModel()
+        {
+            mainViewModel = new MainViewModel();
+            SetCurrentUser();
+        }
+
+        private async void SetCurrentUser() {
+            currentUser =  await mainViewModel.GetUserLoggedInAsync();
+        }
+
+        [RelayCommand]
+        public async Task LogoutUserAsync()
+        {
+            //await Shell.Current.GoToAsync("..");
+            await mainViewModel.LogoutUserAsync();
+        }
+    }
+}
