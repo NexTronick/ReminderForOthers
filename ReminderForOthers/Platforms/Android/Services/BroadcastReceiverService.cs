daaa1b9a980bc877515fc281fb8a643abdfebbfa
Natraj Padwani <nextronickk@gmail.com>
2023-04-07 21:48:58 +0800

    BroadcastReceiverService added. After turning on phone, foreground service will run.

---
+++ ReminderForOthers/Platforms/Android/Services/BroadcastReceiverService.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AndroidApp = Android.App.Application;
+
+namespace ReminderForOthers.Platforms.Android.Services
+{
+    [BroadcastReceiver(Enabled =true, Exported =true)]
+    [IntentFilter(new[] {Intent.ActionBootCompleted })]
+    public class BroadcastReceiverService : BroadcastReceiver
+    {
+        public override void OnReceive(Context context, Intent intent)
+        {
+            if(intent.Action == Intent.ActionBootCompleted)
+            {
+                var foregroundServiceIntent = new Intent(AndroidApp.Context, typeof(ForegroundService));
+                AndroidApp.Context.StartForegroundService(intent);
+                context.StartForegroundService(foregroundServiceIntent);
+            }
+        }
+    }
+}
